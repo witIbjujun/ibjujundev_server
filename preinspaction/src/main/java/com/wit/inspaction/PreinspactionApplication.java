25eee1329a9375e1475195e7d87ceda01009e8ee
gram <jjangart5720@naver.com>
2024-12-02 21:00:39 +0900

    설정 변경

--- preinspaction/src/main/java/com/wit/inspaction/PreinspactionApplication.java
+++ preinspaction/src/main/java/com/wit/inspaction/PreinspactionApplication.java
@@ -1,7 +1,5 @@
 package com.wit.inspaction;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
